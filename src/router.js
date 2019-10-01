de86f8d3ec5a85e4daf36e5bd4f7d5e3b9c8a162
piotr-szymak <pitii@poczta.fm>
2019-10-01 15:28:34 +0200

    router setting

--- src/router.js
+++ src/router.js
@@ -13,12 +13,12 @@ export default new Router({
       component: Home
     },
     {
-      path: '/temp',
-      name: 'temp',
+      path: '/humidity',
+      name: 'humidity',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Temp.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/Humidity.vue')
     }
   ]
 })
